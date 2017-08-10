0c41f734f28d4735a87132b27cdf62b633dd7ee9
R.J. Steinert <rj@rjsteinert.com>
2017-08-10 16:13:50 -0400

    Add copying of ckeditor to build so we get all of the files of ckeditor, optimize later

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 polymer build
+rm -r build/default/bower_components/ckeditor
+cp -r bower_components/ckeditor build/default/bower_components/ckeditor
 mv ~/Sites/tangy-sandbox/dat.json ./
 rm -r ~/Sites/tangy-sandbox/*
 mv build/default/* ~/Sites/tangy-sandbox/
